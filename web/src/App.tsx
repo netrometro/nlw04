09bcce66ef8f99336fbf54fd3ece63ba4e2851d2
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-04-03 17:52:45 -0300

    Dia 1

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import { ExperienceBar } from './components/ExpirenceBar';
+
+import './styles/global.css';
+
+export default function App() {
+
+  return (
+    <div className='container'>
+      <ExperienceBar />
+    </div>
+  );
+}
+
